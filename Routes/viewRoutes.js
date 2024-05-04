b415a20b8887074a03a045afefb8e348f620bf63
MustafaAhmad216 <mustafaahmad21666@gmail.com>
2024-05-05 02:01:23 +0300

    nothing serious

--- Routes/viewRoutes.js
+++ Routes/viewRoutes.js
@@ -6,18 +6,18 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-//Every Pages Must Be Accessed By A LoggedIn User
 
-router.get(
-	'/',
-	authController.isLoggedIn,
-	// bookingController.createBookingCheckout,
-	viewsController.getOverview,
-);
+//Every Pages Must Be Accessed By A LoggedIn User
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
-router.get('/my-tours', authController.protect, viewsController.getMyTours);
+router.get(
+	'/my-tours',
+	authController.protect, 
+	// bookingController.createBookingCheckout,
+	viewsController.getMyTours,
+);
 
 
 
